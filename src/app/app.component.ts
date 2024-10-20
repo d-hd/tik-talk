f3486b30bc8cacd18ed35d10541bb1db05af5447
Dmitriy <dmtr.hdj@gmail.com>
2024-10-20 21:50:55 +0300

    Добавление роутов. Добавление страниц авторизации, поиска и профиля. Добавление компонента layout и компонента sidebar. Стилизация авторизации

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,14 @@
-import { ProfileService } from './data/services/profile.service';
-import { Component, inject } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from "./common-ui/profile-card/profile-card.component";
-import { Profile } from './data/interfaces/profile.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ProfileCardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  profileService = inject(ProfileService);
-  profiles: Profile[] = []
 
-  constructor() {
-    this.profileService.getTestAccounts()
-    .subscribe(val => {
-      this.profiles = val
-    })
-  }
 }
